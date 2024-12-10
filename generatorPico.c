b249ee554f9d5c36a64d4d3c658dcedbe033365c
Jib Ray <jib2ray@gmail.com>
2024-12-10 11:37:31 -0800

    Added rms() and current parameter

--- generatorPico.c
+++ generatorPico.c
@@ -11,9 +11,24 @@
 #include "hardware/adc.h"
 #include "pico/time.h"
 
-static const int VERSION = 4;
+static const int VERSION = 6;
 
-#define BAUDRATE 115200
+#define BAUDRATE            115200
+#define CYCLE_COUNT             10 // The number of 60 Hz cycles to sample.
+#define SAMPLES_PER_SECOND  100000
+#define SAMPLE_COUNT        ((CYCLE_COUNT * SAMPLES_PER_SECOND) / 60)
+
+struct Waveform {
+    float rms;
+    int crossings[SAMPLE_COUNT];
+    int crossings_count;
+};
+
+struct Power {
+    float voltage;
+    float current;
+    float frequency;
+};
 
 // GPIO pins.
 static const uint SYSTEM_LED_PIN = 25;
@@ -21,21 +36,16 @@ static const uint DEBUG_PIN = 22;
 
 // ADC channels.
 static const uint POWER_VOLTAGE = 0;
+static const uint POWER_CURRENT = 1;
 static const uint BATTERY_VOLTAGE = 2;
 
-static const float VOLTAGE_SCALE = 0.173043;
+static const float VOLTAGE_SCALE = 0.170079;
+static const float CURRENT_SCALE = 30.4387e-3;
 
 static const uint32_t EXPIRED = 1 << 31;
 
-static const int POWER_SAMPLE_COUNT = 16667;
-static const int POWER_SAMPLE_PERIOD = 10; // In microsends.
-
-static const float UPDATE_PERIOD = 10e6;
-
-struct Power {
-    float voltage;
-    float frequency;
-};
+static const uint32_t SAMPLE_PERIOD = 1000000 / SAMPLES_PER_SECOND; // In microseconds.
+static const uint32_t UPDATE_PERIOD = 10000000; // In microseconds.
 
 int init_serial2(void) {
     // Initialize UART0.
@@ -52,6 +62,7 @@ int init_serial2(void) {
 int init_adc(void) {
     adc_init();
     adc_gpio_init(26);  // ADC channel 0.
+    adc_gpio_init(27);  // ADC channel 1.
     adc_gpio_init(28);  // ADC channel 2.
     adc_select_input(0);
 
@@ -77,75 +88,98 @@ int init_io(void) {
     return 0;
 }
 
-/* Capture a sample of the generator voltage output and compute RMS voltage
- * and frequency. Generator output voltage is 230 volt RMS. The corresponding
- * peak-to_peak voltage is 650 volts. The input range at the ADC is 0 to 
- * 3.3 volts. ADC values are 0 to 4095. The 650 volt input is scaled to
- * a count of 3000. The sample period is 10 usecs. The sampling captures
- * 10 cycles of the 60 Hz generator output.
+/* Capture a sample of the waveform at adc_channel and compute the RMS value
+ * and zero crossings. Returned rms value is scaled by scale_factor.
  */
-struct Power get_power_parameters (void) {
-    struct Power power;
-    int32_t total = 0, base_line, prev_value, buffer[POWER_SAMPLE_COUNT + 1];
+struct Waveform rms(uint adc_channel,  float scale_factor) {
+    struct Waveform waveform;
+    int32_t total = 0, base_line, prev_value, buffer[SAMPLE_COUNT + 1];
     int32_t crossing_sum = 0;
     float squares = 0.0, period;
-    int i, crossings[POWER_SAMPLE_COUNT], crossing_index = 0, crossing_count = 0;
+    int i, crossing_index = 0, crossings_count = 0;
     uint32_t t;
 
-    adc_select_input(POWER_VOLTAGE);
-    for (i = 0; i < POWER_SAMPLE_COUNT; ++i) {
+    // Capture a sample of the waveform.
+    adc_select_input(adc_channel);
+    for (i = 0; i < SAMPLE_COUNT; ++i) {
         t = time_us_32();
         buffer[i] = adc_read();
-        while(time_us_32() - t < POWER_SAMPLE_PERIOD) ;
+        while(time_us_32() - t < SAMPLE_PERIOD) ;
     }
 
     // Compute base line.
-    for (i = 0; i < POWER_SAMPLE_COUNT; ++i) {
+    for (i = 0; i < SAMPLE_COUNT; ++i) {
         total += buffer[i];
     }
-    base_line = total / POWER_SAMPLE_COUNT;
+    base_line = total / SAMPLE_COUNT;
     // printf("Base line = %d\n", base_line);
 
     // Subtract base line from all ADC values;
-    for (i = 0; i < POWER_SAMPLE_COUNT; ++i) {
+    for (i = 0; i < SAMPLE_COUNT; ++i) {
         buffer[i] = buffer[i] - base_line;
         // printf("%d\n", buffer[i]);
     }
 
-    // Convert ADC values to voltage squared and compute the total (sum of squares).
     // printf("Crossings: ");
     prev_value = buffer[0];
-    for (i = 0; i < POWER_SAMPLE_COUNT; ++i) {
-        squares += pow((float)buffer[i] * VOLTAGE_SCALE, 2.0);
+    waveform.crossings_count = 0;
+    for (i = 0; i < SAMPLE_COUNT; ++i) {
+        // Accumulate the square of each voltage sample.
+        squares += pow((float)buffer[i] * scale_factor, 2.0);
 
         // Test for zero crossing.
         if ((prev_value * buffer[i]) < 0) {
-            crossings[crossing_index++] = i;
+            waveform.crossings[crossing_index++] = i;
             prev_value = buffer[i];
             // printf("%d ", i);
-            crossing_count += 1;
+            waveform.crossings_count += 1;
         }
     }
     // printf("\n");
 
-    // Compute the RMS voltage.
-    power.voltage = sqrt(squares / (float)POWER_SAMPLE_COUNT);
+    waveform.rms = sqrt(squares / (float)SAMPLE_COUNT);
+    return waveform;
+}
 
-    // Calculated the frequency by computing the average distance between zero
+/* Capture a sample of the generator voltage output and compute RMS voltage
+ * and frequency. Generator output voltage is 230 volt RMS. The corresponding
+ * peak-to_peak voltage is 650 volts. The input range at the ADC is 0 to
+ * 3.3 volts. ADC values are 0 to 4095. The 650 volt input is scaled to
+ * a count of 3000. The sample period is 10 usecs. The sampling captures
+ * CYCLE_COUNT cycles of the 60 Hz generator output.
+ */
+struct Power get_power_parameters (void) {
+    struct Power power;
+    struct Waveform waveform;
+    int32_t total = 0, base_line, prev_value;
+    int32_t crossing_sum = 0;
+    float squares = 0.0, period;
+    int i;
+    uint32_t t;
+
+    // Sample the voltage channel.
+    waveform = rms(POWER_VOLTAGE, VOLTAGE_SCALE);
+    power.voltage = waveform.rms;
+
+    // Calculate the frequency by computing the average distance between zero
     // crossings.
-    if (crossing_count > 10 && crossing_count < 100) {
-        prev_value = crossings[0];
-        for (i = 1; i < crossing_index; ++i) {
-            crossing_sum += crossings[i] - prev_value;
-            prev_value = crossings[i];
+    if (waveform.crossings_count > 10 && waveform.crossings_count < 100) {
+        prev_value = waveform.crossings[0];
+        for (i = 1; i < waveform.crossings_count; ++i) {
+            crossing_sum += waveform.crossings[i] - prev_value;
+            prev_value = waveform.crossings[i];
         }
-        period = 1.053 * (float)POWER_SAMPLE_PERIOD * 1e-6 * (float)crossing_sum
-            / (float)crossing_index;
+        period = 1.053 * (float)SAMPLE_PERIOD * 1e-6 * (float)crossing_sum
+            / (float)waveform.crossings_count;
 
         power.frequency = 1.0 / (2.0 * period);
     } else {
         power.frequency = 0.0;
     }
+
+    // Sample the current channel.
+    waveform = rms(POWER_CURRENT, CURRENT_SCALE);
+    power.current = waveform.rms;
 
     return power;
 }
@@ -166,10 +200,10 @@ float get_battery_voltage(void) {
 int send_parameters(struct Power power_parameters, float battery_voltage) {
     char buffer[80];
 
-    // Send generator voltage, generator frequency and battery voltage.
+    // Send generator voltage, generator frequency, generator current and battery voltage.
     // Values have a '!' prefix.
-    sprintf(buffer, "!%0.2f %0.2f %0.2f\n", power_parameters.voltage,
-            power_parameters.frequency, battery_voltage);
+    sprintf(buffer, "!%0.2f %0.2f %0.2f %0.2f\n", power_parameters.voltage,
+            power_parameters.frequency, power_parameters.current, battery_voltage);
     uart_puts(uart0, buffer);
 
     return 0;
@@ -191,7 +225,8 @@ int main(void) {
             battery_voltage = get_battery_voltage();
             printf("Battery voltage  = %0.2f, ", battery_voltage);
             power = get_power_parameters();
-            printf("Power voltage = %0.2f, frequency = %0.2f\n", power.voltage, power.frequency);
+            printf("Power voltage = %0.2f, current = %0.2f, frequency = %0.2f\n",
+                    power.voltage, power.current, power.frequency);
             send_parameters(power, battery_voltage);
             gpio_put(SYSTEM_LED_PIN, 0);
         }
